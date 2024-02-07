f4b7fe58aa0cff06b0e9043bf3225d83054ecc8b
SKY <onthsky@gmail.com>
2024-02-07 20:59:55 +0300

    Project has done

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        IPersonService _personService;
+
+        public PttManager(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        public void GiveMask(Person person)
+        {
+            if(_personService.CheckPerson(person))
+                Console.WriteLine("Maskelendi");
+            else
+                Console.WriteLine("Maskelenemedi !");
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PttManager manager = new(new PersonManager());
+            manager.GiveMask(new Person { FirstName="Sıttık Kemal2", LastName="Yoğurt", NationalIdentity=38620461864, DateOfBirthYear=2000});
+            Console.ReadLine();
+        }
+    }
+}
